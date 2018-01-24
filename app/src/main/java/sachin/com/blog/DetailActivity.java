8e8f63d9e5e47026c3b56ea7507e0a7678fe7f6e
Sachin Kr Pandey <developersachinkr@gmail.com>
2018-01-24 14:51:18 +0530

    hiding header webview

--- app/src/main/java/sachin/com/blog/DetailActivity.java
+++ app/src/main/java/sachin/com/blog/DetailActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -28,6 +31,11 @@ public class DetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         webView.setVisibility(View.INVISIBLE);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClient() {
             @Override
@@ -44,10 +52,17 @@ public class DetailActivity extends AppCompatActivity {
                 Toast.makeText(DetailActivity.this, "page loaded", Toast.LENGTH_SHORT).show();
 
                 // hide header of webpage
-                //String javaScript ="javascript:(function() { var a= document.getElementsByTagName('header');a[0].hidden='true';a=document.getElementsByClassName('page_body');a[0].style.padding='0px';})()";
-                // webView.loadUrl(javaScript);
+                String javaScript = "javascript:(function() { var a= document.getElementsByTagName('header');a[0].hidden='true';a=document.getElementsByClassName('page_body');a[0].style.padding='0px';})()";
+                webView.loadUrl(javaScript);
 
             }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                Toast.makeText(DetailActivity.this, "Error Loading Page", Toast.LENGTH_LONG).show();
+            }
+
+
         });
         webView.loadUrl(getIntent().getStringExtra("url"));
 
